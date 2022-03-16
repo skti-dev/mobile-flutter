8466c83e19c6ca5b6b75c8b5b5ef35aa7f45edde
Augusto Seabra <augusto.seabra00@gmail.com>
2022-03-16 20:41:37 -0300

    Adicionando app fluter

---
+++ app_01/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: widgetHome(),
+    );
+  }
+}
+
+class widgetHome extends StatelessWidget {
+  const widgetHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("App Hello"),
+        centerTitle: true,
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: Text("Hello Turma!"),
+        ),
+      ),
+    );
+  }
+}
